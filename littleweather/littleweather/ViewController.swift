c326ed0c4db0258fdd3a345ed10f035c6cc23697
Nicholas Grana <grananicholas@gmail.com>
2022-01-19 19:47:03 -0500

    initial project commit

---
+++ littleweather/littleweather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  littleweather
+//
+//  Created by Nicholas Grana on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
