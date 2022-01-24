1b1e8ab6ff81e77b2e9d4423413e253cc3b11635
Nicholas Grana <grananicholas@gmail.com>
2022-01-23 23:24:21 -0500

    resolve issues with adding or removing cities

--- littleweather/littleweather/WeatherScrollViewController.swift
+++ littleweather/littleweather/WeatherScrollViewController.swift
@@ -10,13 +10,7 @@ import Firebase
 
 class WeatherScrollViewController: UIViewController {
     
-    var cities = [String]() {
-        didSet {
-            deleteCityButton.isEnabled = cities.count > 1
-            Database.database().reference().child("nick").setValue(cities)
-            setup(with: cities)
-        }
-    }
+    var cityViews = [CityWeatherView]()
     
     @IBOutlet weak var citiesPageControl: UIPageControl!
     @IBOutlet weak var deleteCityButton: UIButton!
@@ -47,24 +41,57 @@ class WeatherScrollViewController: UIViewController {
         view.bringSubviewToFront(deleteCityButton)
         view.bringSubviewToFront(addCityButton)
 
-        deleteCityButton.isEnabled = cities.count > 1
+        deleteCityButton.isEnabled = cityViews.count > 1
         
         Database.database().reference().child("nick").observeSingleEvent(of: .value, with: { snapshot in
             if let id = snapshot.value as? Array<String?> {
-                self.cities = id.filter({ $0 != nil }) as! Array<String>
+                let cities = id.filter({ $0 != nil }) as! Array<String>
+                self.setup(with: cities)
             }
         })
     }
     
-    func setup(with cities: [String]) {
-        citiesScrollView.contentSize = CGSize(width: view.frame.width * CGFloat(cities.count), height: view.frame.height)
-        citiesScrollView.subviews.forEach({ $0.removeFromSuperview() })
-        citiesPageControl.numberOfPages = cities.count
+    func create(city: String, at index: Int, withCompletion completion: @escaping (CityWeatherView?) -> ()) {
+        let cityView = Bundle.main.loadNibNamed("CityWeatherView", owner: self, options: nil)?.first as! CityWeatherView
+        cityView.frame = CGRect(x: view.frame.width * CGFloat(index), y: 0, width: view.frame.width, height: view.frame.height)
+        cityView.renameCity = { [self] city in
+            enterCityAlert(replacementIndex: index)
+        }
         
+        getWeatherResponse(forCity: city) { response, error in
+            if let error = error {
+                completion(nil)
+                print(error)
+            } else {
+                cityView.weather = response
+                completion(cityView)
+            }
+        } imageCompletion: { image in
+            if let image = image {
+                cityView.mainImage.image = image
+            }
+        }
+    }
+    
+    func commitChanges() {
+        let cityNames = cityViews.map { $0.cityName }
+        Database.database().reference().child("nick").setValue(cityNames)
+        deleteCityButton.isEnabled = cityViews.count > 1
+    }
+    
+    func updateScrollViewCitiesSize() {
+        citiesScrollView.contentSize = CGSize(width: view.frame.width * CGFloat(cityViews.count), height: view.frame.height)
+        citiesPageControl.numberOfPages = cityViews.count
+    }
+    
+    func setup(with cities: [String]) {
         for (i, city) in cities.enumerated() {
-            createCityView(index: i, city: city) { cityWeatherView in
-                if let weatherCity = cityWeatherView {
-                    self.citiesScrollView.insertSubview(weatherCity, at: i)
+            create(city: city, at: i) { cityView in
+                if let cityView = cityView {
+                    self.citiesScrollView.addSubview(cityView)
+                    self.cityViews.append(cityView)
+                    self.updateScrollViewCitiesSize()
+                    self.commitChanges()
                 }
             }
         }
@@ -76,8 +103,12 @@ class WeatherScrollViewController: UIViewController {
     }
     
     @IBAction func deleteCity(_ sender: UIButton) {
-        if (cityIndex < cities.count) {
-            cities.remove(at: cityIndex)
+        if (cityIndex < cityViews.count) {
+            cityViews[cityIndex].removeFromSuperview()
+            cityViews.remove(at: cityIndex)
+            
+            self.commitChanges()
+            self.updateScrollViewCitiesSize()
         }
     }
     
@@ -85,7 +116,7 @@ class WeatherScrollViewController: UIViewController {
         enterCityAlert()
     }
     
-    func enterCityAlert(replaceCity: String? = nil) {
+    func enterCityAlert(replacementIndex: Int? = nil) {
         let alert = UIAlertController(title: "Enter City", message: nil, preferredStyle: .alert)
         
         var inputField: UITextField!
@@ -97,17 +128,32 @@ class WeatherScrollViewController: UIViewController {
         
         let done = UIAlertAction(title: "Done", style: .default) { action in
             if let city = inputField.text?.replacingOccurrences(of: " ", with: "%20") {
-                self.createCityView(index: self.cityIndex, city: city) { cityWeatherView in
-                    if let _ = cityWeatherView {
-                        if let replaceCity = replaceCity {
-                            if let replaceIndex = self.cities.firstIndex(of: replaceCity) {
-                                self.cities[replaceIndex] = city
-                            }
-                        } else {
-                            self.cities.insert(city, at: self.cityIndex + 1)
-                        }
-                    } else {
+                
+                self.getWeatherResponse(forCity: city) { response, error in
+                    if let _ = error {
                         self.presentError(text: "City not found!")
+                    } else {
+                        if let replacementIndex = replacementIndex {
+                            self.cityViews[replacementIndex].weather = response
+                            self.commitChanges()
+                        } else {
+                            let newCityIndex = self.cityIndex + 1
+                            self.create(city: city, at: newCityIndex) { cityView in
+                                if let cityView = cityView {
+                                    self.citiesScrollView.insertSubview(cityView, at: newCityIndex)
+                                    self.cityViews.insert(cityView, at: newCityIndex)
+                                    self.updateScrollViewCitiesSize()
+                                    
+                                    let newCityPoint = CGPoint(x: CGFloat(newCityIndex) * self.citiesScrollView.frame.width, y: 0)
+                                    self.citiesScrollView.setContentOffset(newCityPoint, animated: false)
+                                    self.commitChanges()
+                                }
+                            }
+                        }
+                    }
+                } imageCompletion: { image in
+                    if let image = image, let replacementIndex = replacementIndex {
+                        self.cityViews[replacementIndex].mainImage.image = image
                     }
                 }
             }
@@ -122,28 +168,22 @@ class WeatherScrollViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func createCityView(index: Int, city: String, completion: @escaping (CityWeatherView?) -> ()) {
-        let cityView = Bundle.main.loadNibNamed("CityWeatherView", owner: self, options: nil)?.first as! CityWeatherView
-        cityView.frame = CGRect(x: view.frame.width * CGFloat(index), y: 0, width: view.frame.width, height: view.frame.height)
-        cityView.renameCity = { [self] city in
-            enterCityAlert(replaceCity: city)
-        }
-        
+    func getWeatherResponse(forCity city: String, completion: @escaping ((WeatherResponse?, Error?) -> ()), imageCompletion: @escaping ((UIImage?) -> ())) {
         weather.getWeather(for: city) { weatherResponse, error in
             if let weatherResponse = weatherResponse {
-                cityView.weather = weatherResponse
+                completion(weatherResponse, nil)
                 
                 self.weather.getWeatherIcon(id: weatherResponse.weather.first!.icon) { data in
                     if let data = data, let image = UIImage(data: data) {
-                        cityView.mainImage.image = image
+                        imageCompletion(image)
+                    } else {
+                        imageCompletion(nil)
                     }
-                    
-                    completion(cityView)
                 }
-
             } else if let error = error {
                 print(error)
-                completion(nil)
+                completion(nil, error)
+                imageCompletion(nil)
             }
         }
     }
